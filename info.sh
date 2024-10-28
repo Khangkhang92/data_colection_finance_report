265475de658feaeb0a1de191637275b91e9c8626
khanglv <khanglv@vnpost.vn>
2024-10-28 15:37:33 +0700

    update_quote server

--- info.sh
+++ info.sh
@@ -6,5 +6,6 @@ cd /home/shota72fx/data_colection_finance_report
 source /home/shota72fx/miniconda3/bin/activate data
 
 
-python /home/shota72fx/data_colection_finance_report/market_metion.py > /home/shota72fx/data_colection_finance_report/market_metion.log 2>&1
-python /home/shota72fx/data_colection_finance_report/posts.py > /home/shota72fx/data_colection_finance_report/posts.log 2>&1
+python /home/shota72fx/data_colection_finance_report/market_mention.py > /home/shota72fx/data_colection_finance_report/logs/market_mention.log 2>&1
+python /home/shota72fx/data_colection_finance_report/posts.py > /home/shota72fx/data_colection_finance_report/logs/posts.log 2>&1
+python /home/shota72fx/data_colection_finance_report/update_quote.py > /home/shota72fx/data_colection_finance_report/logs/update_quote.log 2>&1
