4ad526076176ef6b1725dfff759fcab09fbcfa0e
khanglv <khanglv@vnpost.vn>
2024-11-11 16:03:47 +0700

    update daily call

--- info.sh
+++ info.sh
@@ -6,6 +6,6 @@ cd /home/shota72fx/data_colection_finance_report
 source /home/shota72fx/miniconda3/bin/activate data
 
 
-python /home/shota72fx/data_colection_finance_report/market_mention.py > /home/shota72fx/data_colection_finance_report/logs/market_mention.log 2>&1
-python /home/shota72fx/data_colection_finance_report/posts.py > /home/shota72fx/data_colection_finance_report/logs/posts.log 2>&1
-python /home/shota72fx/data_colection_finance_report/update_quote.py > /home/shota72fx/data_colection_finance_report/logs/update_quote.log 2>&1
+python /home/shota72fx/data_colection_finance_report/market_mention.py > /home/shota72fx/data_colection_finance_report/logs/market_mention.log 2>&1 // market share symbol
+python /home/shota72fx/data_colection_finance_report/posts.py > /home/shota72fx/data_colection_finance_report/logs/posts.log 2>&1 // news
+python /home/shota72fx/data_colection_finance_report/session_quote.py > /home/shota72fx/data_colection_finance_report/logs/session_quote.log 2>&1
