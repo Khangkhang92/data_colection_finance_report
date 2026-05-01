54f1332259ca0d95935817c1b685dd57550d35a0
khangkhang92 <khang.luongvan18@gmail.com>
2026-05-01 14:06:10 +0700

    update celery

--- finance_schema_lib/finance_schema/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Shared finance database schema package."""
-
-__version__ = "0.1.0"
--- postgrest_funtion/guidebackup.sql
+++
@@ -1,8 +0,0 @@
-pg_dump -U your_username -Fc your_database_name > your_backup_file.dump
-INSERT INTO symbol (ticker, exchange, company_name, industry, sector, short_industry, cap_ratio) VALUES
-('USD-VND', 'FOREX', NULL, NULL, NULL, NULL, NULL),
-('VNINDEX', 'HSX', NULL, NULL, NULL, NULL, NULL),
-('VN30', 'HSX', NULL, NULL, NULL, NULL, NULL),
-('HNXINDEX', 'HNX', NULL, NULL, NULL, NULL, NULL),
-('UPINDEX', 'UPCOM', NULL, NULL, NULL, NULL, NULL),
-('HNX30', 'HNX', NULL, NULL, NULL, NULL, NULL);
